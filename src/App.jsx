92049b740e0c14199e0dfa1e76437d7cde86e022
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-16 15:23:08 +0300

    Import BestSeller.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import ProductCard from './components/Product';
 import ContactForm from './components/Contact';
 import SignUp from './components/SignUp';
 import {Routes, Route} from 'react-router-dom'
-import Product1 from "./components/Product1";
+import Product1 from "./components/ProductDetail";
+import BestSeller from './components/BestSeller';
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       <Routes>
       <Route path='/' element={<LoginForm/>}></Route>
       <Route path='Product' element={<ProductCard/>}></Route>
+      <Route path='Featured' element={<BestSeller/>}></Route>
       <Route path='Contact' element={<ContactForm/>}></Route>
       <Route path='SignUp' element={<SignUp/>}></Route>
       <Route path='/product/:id' element={<Product1/>}></Route>
