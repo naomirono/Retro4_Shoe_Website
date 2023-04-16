29ca140533909c72510a91f50e6650d159149b16
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-16 13:12:35 +0300

    import ProductDetail.jsx add it to router tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductCard from './components/Product';
 import ContactForm from './components/Contact';
 import SignUp from './components/SignUp';
 import {Routes, Route} from 'react-router-dom'
+import Product1 from "./components/Product1";
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
       <Route path='Product' element={<ProductCard/>}></Route>
       <Route path='Contact' element={<ContactForm/>}></Route>
       <Route path='SignUp' element={<SignUp/>}></Route>
+      <Route path='/product/:id' element={<Product1/>}></Route>
     </Routes> 
     </div>
   )
