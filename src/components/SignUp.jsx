6b7231a446797552c2d4d1a3aa15dc8a73db72b4
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-16 15:17:51 +0300

    Change div background image

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Shoe from '../assets/shoe3.JPG'
+import Shoe from '../assets/shoe4.JPG'
 
 const SignUp = () => {
   return (
-    <div className='flex justify-center items-center mx-auto h-screen bg-black bg-contain bg-center bg-no-repeat w-full h-[88vh]' style={{backgroundImage:`url(${Shoe})`, backgroundPosition: 'center', backgroundSize: 'contain' }}>
+    <div className='flex justify-center items-center mx-auto h-screen bg-white bg-contain bg-center bg-no-repeat w-full h-[87.5vh]' style={{backgroundImage:`url(${Shoe})`, backgroundPosition: 'center', backgroundSize: 'contain' }}>
     
-    
-  
-   
-
     </div>
 
   )
