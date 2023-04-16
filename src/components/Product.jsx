b4175c5299d35765310807c9f2a88f826f2a3f07
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-16 13:11:06 +0300

    import link tag and add it to return statement

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function ProductCard() {
   const [products, setProducts] = useState([]);
@@ -14,20 +15,31 @@ function ProductCard() {
 
   return (
     <div className="flex flex-wrap justify-center py-8 max-w-[1240px] mx-auto">
-  {products.map((product) => (
-    <div key={product.id} className="w-full sm:w-1/2 lg:w-1/3 p-4 flex">
-      <div className=" p-4 flex-1 flex flex-col ">
-      <div className="bg-white rounded-lg overflow-hidden">
-          <img src={product.image} alt={product.description} className="w-full mb-4 " style={{ transform: 'rotate(-30deg)'}}  />
-        </div>
-        <h2 className="text-lg font-semibold mb-2 text-[#f2f3f4] text-center mt-10">{product.description}</h2>
-        <div className="flex justify-center mt-auto ">
-          <p className="text-gray-600">${product.price}</p>
-        </div>
+    {products.map((product) => (
+      <div key={product.id} className="w-full sm:w-1/2 lg:w-1/3 p-4 flex">
+        <Link to={`/product/${product.id}`} className=" p-4 flex-1 flex flex-col ">
+         
+            <div className="bg-white rounded-lg overflow-hidden">
+              <img
+                src={product.image}
+                alt={product.description}
+                className="w-full mb-4"
+                style={{ transform: "rotate(-30deg)", height: "100%" }}
+              />
+            </div>
+            <h2 className="text-lg font-semibold mb-2 text-[#f2f3f4] text-center mt-10">
+              {product.description}
+            </h2>
+            <div className="flex justify-center mt-auto ">
+              <p className="text-gray-600">${product.price}</p>
+            </div>
+          
+        </Link>
       </div>
-    </div>
-  ))}
-</div>
+    ))}
+  </div>
+  
+  
   );
 }
 
