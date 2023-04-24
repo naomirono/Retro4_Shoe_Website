7111b788a9d747076af07fc2e6e718d9b7e8c9b9
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-24 22:24:10 +0300

    Create a productPage component to display both productCard and Review component

---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductCard from './Product';
+import Review from './ProductReview';
+
+
+const ProductPage = () => {
+  return (
+    <div>
+    <ProductCard/>
+    <Review/>
+
+    </div>
+  )
+}
+
+export default ProductPage;
