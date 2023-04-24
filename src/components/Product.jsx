eab056ce04ee8140f9a5ef328ae2a4a0cb1baa7d
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-24 22:22:31 +0300

    Add product title

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -14,35 +14,39 @@ function ProductCard() {
   }, []);
 
   return (
-    <div className="flex flex-wrap justify-center py-8 max-w-[1240px] mx-auto">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="w-full sm:w-1/2 lg:w-1/3 p-4 flex hover:scale-105 transition-all duration-500 cursor-pointer"
-        >
-          <Link
-            to={`/product/${product.id}`}
-            className=" p-4 flex-1 flex flex-col "
+    <div className="max-w-[1240px] mx-auto">
+      <h2 className="text-center text-3xl font-bold my-5 text-white">Latest <span className="text-orange-400">Products</span></h2>
+      <div className="flex flex-wrap justify-center py-8">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="w-full sm:w-1/2 lg:w-1/3 p-4 flex hover:scale-105 transition-all duration-500 cursor-pointer"
           >
-            <div className="bg-white rounded-lg overflow-hidden">
-              <img
-                src={product.image}
-                alt={product.description}
-                className="w-full mb-4"
-                style={{ transform: "rotate(-30deg)", height: "100%" }}
-              />
-            </div>
-            <h2 className="text-lg font-semibold mb-2 text-[#f2f3f4] text-center mt-10">
-              {product.description}
-            </h2>
-            <div className="flex justify-center mt-auto ">
-              <p className="text-gray-600">${product.price}</p>
-            </div>
-          </Link>
-        </div>
-      ))}
+            <Link
+              to={`/product/${product.id}`}
+              className="p-4 flex-1 flex flex-col"
+            >
+              <div className="bg-white rounded-lg overflow-hidden">
+                <img
+                  src={product.image}
+                  alt={product.description}
+                  className="w-full mb-4"
+                  style={{ transform: "rotate(-30deg)", height: "100%" }}
+                />
+              </div>
+              <h2 className="text-lg font-semibold mb-2 text-[#f2f3f4] text-center mt-10">
+                {product.description}
+              </h2>
+              <div className="flex justify-center mt-auto ">
+                <p className="text-gray-600">${product.price}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
+  
 }
 
 export default ProductCard;
